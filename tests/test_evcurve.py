bf5f1512e3428ab8e7171780a90d41d2baae65a5
Jan Janssen <janssen@lanl.gov>
2023-08-14 10:50:43 -0600

    Fix bulk modulus

--- tests/test_evcurve.py
+++ tests/test_evcurve.py
@@ -9,8 +9,8 @@ def validate_fitdict(fit_dict):
     lst = [
         fit_dict['b_prime_eq'] > 2.2,
         fit_dict['b_prime_eq'] < 2.5,
-        fit_dict['bulkmodul_eq'] > 177,
-        fit_dict['bulkmodul_eq'] < 188,
+        fit_dict['bulkmodul_eq'] > 175,
+        fit_dict['bulkmodul_eq'] < 180,
         fit_dict['energy_eq'] > -453.9,
         fit_dict['energy_eq'] < -453.6,
         fit_dict['volume_eq'] > 1207,
