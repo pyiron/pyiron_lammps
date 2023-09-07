7a458c8093b900a6fb75a3717a3a0a1396a5c72a
Jan Janssen <jan.janssen@outlook.com>
2023-09-07 12:05:10 +0200

    Update structure toolkit and atomistics to 0.0.11 and 0.0.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
     install_requires=[
         'ase==3.22.1',
-        'atomistics==0.0.1',
+        'atomistics==0.0.3',
         'numpy==1.23.5',
         'pandas==2.1.0',
         'pylammpsmpi==0.2.2',
