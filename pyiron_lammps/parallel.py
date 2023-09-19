ea591f8fc9ac8c41e381b43f6dff9c12f45338e7
Jan Janssen <jan.janssen@outlook.com>
2023-09-19 17:00:20 +0200

    bug fix

--- pyiron_lammps/parallel.py
+++ pyiron_lammps/parallel.py
@@ -45,10 +45,10 @@ def _parallel_execution(function, input_parameter_lst, cores=1):
         ]
     elif cores > 1:
         with PyMPIExecutor(max_workers=cores) as p:
-            return p.map(
+            return list(p.map(
                 function,
                 [input_parameter + [True] for input_parameter in input_parameter_lst],
-            )
+            ))
     else:
         raise ValueError("The number of cores has to be a positive integer.")
 
