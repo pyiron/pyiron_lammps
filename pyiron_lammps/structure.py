6d0220afd4254189c31f1c5780313475a23d211b
Jan Janssen <janssen@mpie.de>
2025-11-20 16:45:39 -0800

    Add support for atom_type selection

--- pyiron_lammps/structure.py
+++ pyiron_lammps/structure.py
@@ -505,8 +505,9 @@ def write_lammps_datafile(
     units: str = "metal",
     file_name: str = "lammps.data",
     working_directory: Optional[str] = None,
+    atom_type: str = "atomic",
 ) -> None:
-    lammps_str = LammpsStructure(bond_dict=bond_dict, units=units)
+    lammps_str = LammpsStructure(bond_dict=bond_dict, units=units, atom_type=atom_type)
     lammps_str.el_eam_lst = potential_elements
     lammps_str.structure = structure
     lammps_str.write_file(file_name=file_name, cwd=working_directory)
