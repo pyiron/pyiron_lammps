f3e8f789ffaa0330a4de3b332d4f6ae773f27f93
Jan Janssen <janssen@mpie.de>
2025-11-27 11:08:10 +0100

    modify test

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -15,12 +15,12 @@ class TestConstraints(unittest.TestCase):
         structure.symbols[2:] = "Al"
         cls.structure = structure
 
-    def test_selective_dynamics_mixed_calcmd(self):
+    def test_selective_dynamics_mixed_calcmd_y(self):
         atoms = self.structure.copy()
         c1 = FixAtoms(indices=[atom.index for atom in atoms if atom.symbol == "Cu"])
         c2 = FixedPlane(
             [atom.index for atom in atoms if atom.symbol == "Al"],
-            [1, 0, 0],
+            [0, 1, 0],
         )
         atoms.set_constraint([c1, c2])
         control_dict = set_selective_dynamics(structure=atoms, calc_md=True)
@@ -30,13 +30,13 @@ class TestConstraints(unittest.TestCase):
             control_dict["fix constraintxyz"], "constraintxyz setforce 0.0 0.0 0.0"
         )
         self.assertTrue(control_dict["velocity constraintxyz"], "set 0.0 0.0 0.0")
-        self.assertTrue(control_dict["group constraintx"], "id 3 4")
+        self.assertTrue(control_dict["group constrainty"], "id 3 4")
         self.assertTrue(
-            control_dict["fix constraintx"], "constraintx setforce 0.0 NULL NULL"
+            control_dict["fix constrainty"], "constrainty setforce NULL 0.0 NULL"
         )
-        self.assertTrue(control_dict["velocity constraintx"], "set 0.0 NULL NULL")
+        self.assertTrue(control_dict["velocity constrainty"], "set NULL 0.0 NULL")
 
-    def test_selective_dynamics_mixed(self):
+    def test_selective_dynamics_mixed_x(self):
         atoms = self.structure.copy()
         c1 = FixAtoms(indices=[atom.index for atom in atoms if atom.symbol == "Cu"])
         c2 = FixedPlane(
