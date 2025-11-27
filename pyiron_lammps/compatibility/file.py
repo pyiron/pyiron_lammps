d4b0dd44acfeb52783bcf5de41ddb769339f4aa2
Jan Janssen <janssen@mpie.de>
2025-11-27 09:59:35 +0100

    Add tests and move constraints to separate

--- pyiron_lammps/compatibility/file.py
+++ pyiron_lammps/compatibility/file.py
@@ -8,8 +8,8 @@ from pyiron_lammps.compatibility.calculate import (
     calc_md,
     calc_minimize,
     calc_static,
-    set_selective_dynamics,
 )
+from pyiron_lammps.compatibility.constraints import set_selective_dynamics
 from pyiron_lammps.output import parse_lammps_output
 from pyiron_lammps.potential import get_potential_by_name
 from pyiron_lammps.structure import write_lammps_datafile
